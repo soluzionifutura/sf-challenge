8954a2583381419ae6288cdbb3a8b68c50f4a2ce
Tommaso Pedroni <pedronitom1@gmail.com>
2021-06-22 10:06:31 +0200

    Code Review

--- teams/a/Entity.js
+++ teams/a/Entity.js
@@ -1,15 +1,11 @@
 class Entity{
     constructor(field, x, y){
-        if (x < 0 || x >= field.w || y < 0 || y >= field.h) {
+        if (!field.checkCoords(x,y)) {
             throw new Error("Entità posta fuori dal field")
         }
         
-        if(field.entities.length > 0){
-            field.entities.forEach(e => {
-                if(e.x === x && e.y === y){
-                    throw new Error("Entità posta in una casella già occupata")
-                }
-            })
+        if(field.getEntity(x,y)){
+            throw new Error("Entità posta in una casella già occupata")
         }
         
 
@@ -22,78 +18,38 @@ class Entity{
     }
 
     move(direction) {
+        if(this.moves >= this.field.maxMoves) return 
+
+        let newX = this.x
+        let newY = this.y
+
         switch(direction) {
             case 0:
-                if(this.y - 1 >= 0 && this.moves < this.field.maxMoves){
-                    this.y--   
-                    this.moves++
-                    let index = -1
-                    for(let i = 0; i < this.field.countEntities(); i++){
-                        if(this.field.entities[i] == this) continue
-                        if(this.field.entities[i].x === this.x && this.field.entities[i].y === this.y){
-                           index = i 
-                        }
-                    }
-                    
-                    if (index !== -1) {
-                        this.field.entities.splice(index, 1)
-                    }
-                }      
+                newY--   
                 break
             case 1:
-                if(this.x + 1 < this.field.w && this.moves < this.field.maxMoves){
-                    this.x++   
-                    this.moves++
-                    let index = -1
-                    for(let i = 0; i < this.field.countEntities(); i++){
-                        if(this.field.entities[i] == this) continue
-
-                        if(this.field.entities[i].x === this.x && this.field.entities[i].y === this.y){
-                           index = i 
-                        }
-                    }
-                    
-                    if (index !== -1) {
-                        this.field.entities.splice(index, 1)
-                    }
-                }
+                newX++
                 break
             case 2:
-                if(this.y + 1 < this.field.h && this.moves < this.field.maxMoves){
-                    this.y++   
-                    this.moves++
-                    let index = -1
-                    for(let i = 0; i < this.field.countEntities(); i++){
-                        if(this.field.entities[i] == this) continue
-
-                        if(this.field.entities[i].x === this.x && this.field.entities[i].y === this.y){
-                           index = i 
-                        }
-                    }
-                    
-                    if (index !== -1) {
-                        this.field.entities.splice(index, 1)
-                    }
-                }
+                newY++
                 break
             case 3:
-                if(this.x - 1 >= 0 && this.moves < this.field.maxMoves){
-                    this.x--   
-                    this.moves++
-                    let index = -1
-                    for(let i = 0; i < this.field.countEntities(); i++){
-                        if(this.field.entities[i] == this) continue
-                        if(this.field.entities[i].x === this.x && this.field.entities[i].y === this.y){
-                           index = i 
-                        }
-                    }
-                    
-                    if (index !== -1) {
-                        this.field.entities.splice(index, 1)
-                    }
-                }
+                newX--
                 break
         }
+
+        if(this.field.checkCoords(newX,newY) && this.moves < this.field.maxMoves){
+            this.x = newX
+            this.y = newY
+            this.moves++
+
+            let entity = this.field.getEntity(this.x,this.y,this)
+
+            if(entity) this.field.entities.splice(this.field.entities.indexOf(entity),1)
+            
+        }
+
+        
     }
 
 }
